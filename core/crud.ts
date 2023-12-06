eb07dc42b9b70a7dc9e829f50ca606123db77a7a
Darlley <sytembrtt@gmail.com>
2023-12-06 11:30:15 -0400

    read and clear db function

--- core/crud.ts
+++ core/crud.ts
@@ -1,11 +1,57 @@
 import fs from 'fs';
+import { randomUUID } from 'crypto'
 // const fs = require('fs')
 
 const DB_FILE_PATH = "./core/db"
 
-function create(content: string){
-    fs.writeFileSync(DB_FILE_PATH, content)
-    return content
+interface Todo {
+    id: string,
+    date: string,
+    content: string,
+    done: boolean
 }
 
-console.log(create('Olá mundo'))
+// const todos: Array<Todo> = []
+
+function create(content: string){
+    const todo: Todo = {
+        id: randomUUID(),
+        date: new Date().toISOString(),
+        content: content,
+        done: false
+    }
+
+    // todos.push(todo)
+    const todos: Array<Todo> = [
+        ...read(),
+        todo
+    ]
+
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos, 
+        users: []
+    }, null, 2))
+
+    return content;
+}
+
+function read(): Array<Todo>{
+    const response = fs.readFileSync(DB_FILE_PATH, "utf-8");
+    const data = JSON.parse(response || "{}");
+
+    if(!data.todos){
+        return []; // Fail Fast Validation
+    }
+    
+    return data.todos;
+}
+
+
+function CLEAR_DB(){
+    fs.writeFileSync(DB_FILE_PATH, '');
+}
+
+CLEAR_DB()
+create('Todo 1')
+create('Todo 2')
+console.log(read())

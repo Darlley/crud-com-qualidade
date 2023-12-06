a27995fa3d7eaac1f167635eab16e8b599541596
Darlley <sytembrtt@gmail.com>
2023-12-06 11:58:53 -0400

    update

--- core/crud.ts
+++ core/crud.ts
@@ -8,17 +8,28 @@ interface Todo {
     id: string,
     date: string,
     content: string,
-    done: boolean
+    done: boolean,
+    created_at: string,
+    updated_at: string,
 }
 
 // const todos: Array<Todo> = []
 
-function create(content: string){
+function WRITE_FILE(todos: Array<Todo>){
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos, 
+        users: []
+    }, null, 2))
+}
+
+function create(content: string): Todo{
     const todo: Todo = {
         id: randomUUID(),
         date: new Date().toISOString(),
         content: content,
-        done: false
+        done: false,
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
     }
 
     // todos.push(todo)
@@ -27,12 +38,9 @@ function create(content: string){
         todo
     ]
 
-    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
-        todos, 
-        users: []
-    }, null, 2))
+    WRITE_FILE(todos)
 
-    return content;
+    return todo;
 }
 
 function read(): Array<Todo>{
@@ -46,6 +54,38 @@ function read(): Array<Todo>{
     return data.todos;
 }
 
+function update(id: string, partialTodo: Partial<Todo>){
+    let updated_todo;
+    const todos = read()
+
+    todos.forEach((currentTodo) => {
+        if(currentTodo.id === id){
+            updated_todo = Object.assign(currentTodo, {
+                ...partialTodo,
+                updated_at: new Date().toISOString(),
+            })
+        }
+    })
+
+    WRITE_FILE(todos)
+
+    if(!updated_todo){
+        throw new Error("Please, provide another ID!");
+    }
+
+    return updated_todo;
+}
+
+function updateContentByID(id: string, content: string): Todo {
+    return update(id, {
+        content
+    })
+}
+function updateDoneByID(id: string, done: boolean): Todo {
+    return update(id, {
+        done
+    })
+}
 
 function CLEAR_DB(){
     fs.writeFileSync(DB_FILE_PATH, '');
@@ -54,4 +94,11 @@ function CLEAR_DB(){
 CLEAR_DB()
 create('Todo 1')
 create('Todo 2')
+
+const todo = create('Todo 3')
+update(todo.id, {
+    content: "New content 2",
+    done: true
+})
+
 console.log(read())
